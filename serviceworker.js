f2e8c981a9557d18d0a0adf38fa8450911e1e0a2
Gerome Heath <gerome247@gmail.com>
2024-09-09 05:48:42 +0100

    latest

---
+++ serviceworker.js
@@ -0,0 +1,61 @@
+console.log("I'm a service worker, ready to serve");
+
+const url = ["/", "app.js", "styles.css"];
+
+
+// Prefetching the assets
+globalThis.addEventListener("install", event => {
+    caches.open("pwa").then(cache => {
+        cache.addAll(url);
+    })
+});
+
+// Cache first strategy
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request)  // searching in the cache
+            .then( response => {
+                if (response) {
+                    // The request is in the cache
+                    return response; // cache hit
+                } else {
+                    // We need to go to the network
+                    return fetch(event.request);  // cache miss
+                }
+            })
+    );
+});
+
+// Network first strategy
+self.addEventListener('fetch', event => {
+    event.respondWith(
+      fetch(event.request) // I go to the network ALWAYS
+        .catch( error => {  // if the network is down, I go to the cache
+            return caches.open("assets")
+                    .then( cache => {
+                         return cache.match(request);
+                 });
+        })
+    );
+  });
+
+
+// State while revalidate strategy
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then( response => {
+                // Even if the response is in the cache, we fetch it
+                // and update the cache for future usage
+                const fetchPromise = fetch(event.request).then(
+                     networkResponse => {
+                        caches.open("assets").then( cache => {
+                            cache.put(event.request, networkResponse.clone());
+                            return networkResponse;
+                        });
+                    });
+                // We use the currently cached version if it's there
+                return response || fetchPromise; // cached or a network fetch
+            })
+        );
+    }); 
